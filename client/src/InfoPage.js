c18a1216817d3796d63c475cd0efd9a7c8e73fca
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2019-11-24 16:30:46 -0600

    Fixed up some Bootstrap rows

--- client/src/InfoPage.js
+++ client/src/InfoPage.js
@@ -5,11 +5,17 @@ class InfoPage extends React.Component {
     render() {
         return (
             <div className="InfoPage container-fluid">
-                <div className="col Info-Left">
+                <div className="row">
+                    <div className="col Info-Left">
+                        <h2>About Hackathons</h2>
+                        <p>Literally Text</p>
+                        <h2>About ArchHacks</h2>
+                        <p>Literally Text</p>
 
-                </div>
-                <div className="col Info-Right">
-                    
+                    </div>
+                    <div className="col Info-Right">
+                        <h2>FAQ</h2>
+                    </div>
                 </div>
             </div>
         )
