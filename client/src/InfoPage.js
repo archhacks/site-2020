130abc6dd00be2507b42bc3de9598475cc51c6e1
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2019-11-24 15:34:03 -0600

    Fixed default export bug

--- client/src/InfoPage.js
+++ client/src/InfoPage.js
@@ -1,3 +1,2 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -14,3 +14,5 @@ class LandingPage extends React.Component {
         )
     }
 }
+
+export default LandingPage;
