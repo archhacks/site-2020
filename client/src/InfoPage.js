a080194fa4bce41a9234d1327c62e4321a7025db
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2019-11-24 16:42:36 -0600

    Responsive Cols!

--- client/src/InfoPage.js
+++ client/src/InfoPage.js
@@ -5,15 +5,15 @@ class InfoPage extends React.Component {
     render() {
         return (
             <div className="InfoPage container">
-                <div className="row">
-                    <div className="col Info-Left">
+                <div className="row h-100">
+                    <div className="col-md-* Info-Left">
                         <h2>About Hackathons</h2>
                         <p>Literally Text</p>
                         <h2>About ArchHacks</h2>
                         <p>Literally Text</p>
 
                     </div>
-                    <div className="col Info-Right">
+                    <div className="col-md-* Info-Right">
                         <h2>FAQ</h2>
                     </div>
                 </div>
