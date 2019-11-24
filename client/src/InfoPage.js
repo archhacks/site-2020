3d2afd3555390250c580259469421b946e23f408
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2019-11-24 16:48:33 -0600

    We're trying

--- client/src/InfoPage.js
+++ client/src/InfoPage.js
@@ -4,16 +4,16 @@ import './App.css';
 class InfoPage extends React.Component {
     render() {
         return (
-            <div className="InfoPage container">
+            <div className="InfoPage container-fluid">
                 <div className="row h-100">
-                    <div className="col-md-* Info-Left">
+                    <div className="col-md-7 Info-Left">
                         <h2>About Hackathons</h2>
                         <p>Literally Text</p>
                         <h2>About ArchHacks</h2>
                         <p>Literally Text</p>
 
                     </div>
-                    <div className="col-md-* Info-Right">
+                    <div className="col-md-6 Info-Right">
                         <h2>FAQ</h2>
                     </div>
                 </div>
