ea161ae2aaa6cb950c361dcfb268028cbe1d3102
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2019-12-01 15:52:21 -0600

    added real text

--- client/src/InfoPage.js
+++ client/src/InfoPage.js
@@ -10,11 +10,11 @@ class InfoPage extends React.Component {
                         <div className="Info-Left-Content">
                             <div className="Info-About-General">
                                 <h2 className="Info-Header">About Hackathons</h2>
-                                <p className="Info-Text">Literally Text</p>
+                                <p className="Info-Text">A hackathon is best described as an “invention marathon”. Anyone who has an interest in technology attends a hackathon to learn, build and share their creations over the course of a weekend in a relaxed and welcoming atmosphere. You don’t have to be a programmer and you certainly don’t have to be majoring in Computer Science.</p>
                             </div>
                             <div className="Info-About-Specific">
                                 <h2 className="Info-Header">About ArchHacks</h2>
-                                <p className="Info-Text">Literally Text</p>
+                                <p className="Info-Text">ArchHacks brings scientists and creatives together, inspiring individuals and teams to create a project with an emphasis on biotechnology. Over 36 hours, you can expect to build something completely new, get free food and swag, and have a chance to network with top recruiters in the industry!</p>
                             </div>
                         </div>
                     </div>
