3efaa6062b907e3926ff16dad8499f29bf7e394b
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2019-12-01 15:10:35 -0600

    Added Flex to Column

--- client/src/InfoPage.js
+++ client/src/InfoPage.js
@@ -4,16 +4,17 @@ import './App.css';
 class InfoPage extends React.Component {
     render() {
         return (
-            <div className="InfoPage container-fluid px-lg-5">
-                <div className="row h-100 mx-lg-n5">
-                    <div className="col-md-7 Info-Col Info-Left px-lg-5">
-                        <h2>About Hackathons</h2>
-                        <p>Literally Text</p>
-                        <h2>About ArchHacks</h2>
-                        <p>Literally Text</p>
-
+            <div className="InfoPage container-fluid">
+                <div className="row h-100">
+                    <div className="col-md-7 Info-Col Info-Left">
+                        <div className="Info-Left-Content">
+                            <h2>About Hackathons</h2>
+                            <p>Literally Text</p>
+                            <h2>About ArchHacks</h2>
+                            <p>Literally Text</p>
+                        </div>
                     </div>
-                    <div className="col-md-5 Info-Col Info-Right px-lg-5">
+                    <div className="col-md-5 Info-Col Info-Right">
                         <h2>FAQ</h2>
                     </div>
                 </div>
