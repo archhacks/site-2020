854c3dd64ef79c62d58611d62227fb2651c76058
Steven Lee <lee.steven@wustl.edu>
2019-12-01 21:09:47 -0600

    update input fields

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -11,13 +11,16 @@ class LandingPage extends React.Component {
             
             <header className="App-header container">
 
-           
                 <img src={Logo} alt="ArchHacks Logo" id="landing-logo" width= "30%"/>
                 <h1 id="heading"><span id="heading-gray">ARCH</span><span id="heading-red">HACKS</span></h1>
-                <p class="landing-gray">Washington University in St. Louis</p>
-                <p class="landing-gray">February 21-23 | St. Louis, Missouri</p>
-                <button id="landing-applyButton">Apply</button>
-              
+                <p className="landing-gray">Washington University in St. Louis</p>
+                <p className="landing-gray">February 21-23 | St. Louis, Missouri</p>
+                {/* <button id="landing-applyButton">Apply</button> */}
+                <form className="form-inline">
+                    <input type="text" placeholder="Enter your email..." id="email-input"/>
+                    <input type="submit" value="Join" id="join-button"/>
+                </form>
+
             </header>
 
         )
