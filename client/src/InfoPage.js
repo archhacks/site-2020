fd2a54a9b05816c9487f64de95d50b8ea1f835b3
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2019-12-07 19:29:08 -0600

    thick

--- client/src/InfoPage.js
+++ client/src/InfoPage.js
@@ -7,18 +7,21 @@ class InfoPage extends React.Component {
             <div className="InfoPage container-fluid">
                 <div className="row h-100 Info-Row">
                     <div className="col-md-7 Info-Col Info-Left">
-                        <div className="Info-Left-Gutter"></div>
-                        <div className="Info-Left-Content">
-                            <div className="Info-About-General">
-                                <h2 className="Info-Header">About Hackathons</h2>
-                                <p className="Info-Text">A hackathon is best described as an “invention marathon”. Anyone who has an interest in technology attends a hackathon to learn, build and share their creations over the course of a weekend in a relaxed and welcoming atmosphere. You don’t have to be a programmer and you certainly don’t have to be majoring in Computer Science.</p>
+                        <div className="Info-Thick">
+                            <div className="Info-Left-Gutter"></div>
+                            <div className="Info-Left-Content">
+                                <div className="Info-About-General">
+                                    <h2 className="Info-Header">About Hackathons</h2>
+                                    <p className="Info-Text">A hackathon is best described as an “invention marathon”. Anyone who has an interest in technology attends a hackathon to learn, build and share their creations over the course of a weekend in a relaxed and welcoming atmosphere. You don’t have to be a programmer and you certainly don’t have to be majoring in Computer Science.</p>
+                                </div>
+                                <div className="Info-About-Specific">
+                                    <h2 className="Info-Header">About ArchHacks</h2>
+                                    <p className="Info-Text">ArchHacks brings together more than 200 students of all backgrounds from around the country for 48 hours of collaboration, problem solving, and building. We will provide a unique opportunity for students to work with resources and companies they cannot find anywhere else. You will have the opportunity to make invaluable connections with corporations, collaborate with friends and, most importantly, develop something that will contribute to the HealthTech community.</p>
+                                </div>
                             </div>
-                            <div className="Info-About-Specific">
-                                <h2 className="Info-Header">About ArchHacks</h2>
-                                <p className="Info-Text">ArchHacks brings together more than 200 students of all backgrounds from around the country for 48 hours of collaboration, problem solving, and building. We will provide a unique opportunity for students to work with resources and companies they cannot find anywhere else. You will have the opportunity to make invaluable connections with corporations, collaborate with friends and, most importantly, develop something that will contribute to the HealthTech community.</p>
-                            </div>
+                            <div className="Info-Left-Gutter"></div>
                         </div>
-                        <div className="Info-Left-Gutter"></div>
+                        
                     </div>
                     <div className="col-md-5 Info-Col Info-Right">
                         <div className="Info-Right-Content">
