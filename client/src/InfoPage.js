e082e1776312269b6e9f2375c44bf8ba6cb32d97
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2019-12-08 16:43:44 -0600

    updating sizes

--- client/src/InfoPage.js
+++ client/src/InfoPage.js
@@ -12,7 +12,7 @@ class InfoPage extends React.Component {
                             <div className="Info-Left-Content">
                                 <div className="Info-About-General">
                                     <h2 className="Info-Header">About Hackathons</h2>
-                                    <p className="Info-Text">A hackathon is best described as an “invention marathon”. Anyone who has an interest in technology attends a hackathon to learn, build and share their creations over the course of a weekend in a relaxed and welcoming atmosphere. You don’t have to be a programmer and you certainly don’t have to be majoring in Computer Science.</p>
+                                    <p className="Info-Text">A hackathon is best described as an “invention marathon.” Anyone who has an interest in technology attends a hackathon to learn, build and share their creations over the course of a weekend in a relaxed and welcoming atmosphere. You don’t have to be a programmer and you certainly don’t have to be majoring in Computer Science.</p>
                                 </div>
                                 <div className="Info-About-Specific">
                                     <h2 className="Info-Header">About ArchHacks</h2>
