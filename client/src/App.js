5387289d55219200a622f4d89b8beaf8c530d409
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2019-12-08 17:34:11 -0600

    react method!

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import MLHBadge from "./MLHBadge.png";
-
+import BG from "./bg.png"
 import LandingPage from './LandingPage.js'
 import InfoPage from './InfoPage.js'
 import SponsorPage from './SponsorPage.js'
@@ -28,7 +28,7 @@ function App() {
       <a id="mlh-trust-badge" style={aStyle} href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=white" target="_blank">
         <img src={MLHBadge} alt="Major League Hacking 2020 Hackathon Season" style={imgStyle}/>
       </a>
-      <img src="/src/bg.jpg" alt="Background"/>
+      <img src={BG} alt="Background"/>
       <div className="LandingPage Page">
           
           <LandingPage />
