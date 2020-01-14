f67579981e59a04a7d88324b652105e891fb08b8
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2020-01-13 23:17:46 -0600

    moved to lNDING page

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -22,7 +22,12 @@ class LandingPage extends React.Component {
            
             
             <header className="App-header container">
-
+                <div className="social_media">
+                    <a href="https://github.com/archhacks"><i class="fab fa-github fa-2x"></i></a>
+                    <a href="https://www.facebook.com/archhacks/"><i class="fab fa-facebook fa-2x"></i></a>
+                    <a href="https://twitter.com/archhacks"><i class="fab fa-twitter fa-2x"></i></a>
+                    <a href="https://medium.com/@ArchHacks"><i class="fab fa-medium-m fa-2x"></i></a>
+                </div>
                 <img src={Logo} alt="ArchHacks Logo" id="landing-logo"/>
                 <h1 className="Landing-Title" id="heading"><span id="heading-gray">ARCH</span><span id="heading-red">HACKS</span></h1>
                 <p className="landing-gray">Washington University in St. Louis</p>
