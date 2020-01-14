b14a6ca78c62a5fe735d5c1e4515afeabe79b9de
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2020-01-13 23:21:49 -0600

    oops

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -19,7 +19,7 @@ class LandingPage extends React.Component {
 
 
         return (
-           
+        <div>   
             <div className="social_media">
                     <a href="https://github.com/archhacks"><i class="fab fa-github fa-2x"></i></a>
                     <a href="https://www.facebook.com/archhacks/"><i class="fab fa-facebook fa-2x"></i></a>
@@ -46,7 +46,7 @@ class LandingPage extends React.Component {
                 </form>
 
             </header>
-
+            </div>
         )
     }
 }
