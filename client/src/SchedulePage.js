6e76c0b6598bd1df7414299589cd426dc01bc033
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2020-02-09 19:18:14 -0600

    s

--- client/src/SchedulePage.js
+++ client/src/SchedulePage.js
@@ -24,7 +24,7 @@ function SchedElm(props){
                 <p className="schedText schedElmName">{props.name}</p>
                 <p className="schedText schedElmLoc">{props.loc}</p>
             </div>
-            <hr></hr>
+            <hr className="schedHR"></hr>
         </div>
     )
 }
