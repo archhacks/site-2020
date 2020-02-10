93f6d3b30a659b27398fe7d7a20527f4a34c8deb
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2020-02-09 20:16:48 -0600

    add pic

--- client/src/SchedulePage.js
+++ client/src/SchedulePage.js
@@ -38,18 +38,21 @@ class SchedulePage extends React.Component {
 
         return (
             <div className="schedulePage">   
-                <div className="schedule">
-                    <h1 className="scheduleTitle">Schedule</h1>
-                    <input type="button" value="General" className="Info-FAQ-Select FAQ-General-B FAQ-Selected-B"/>
-                    <input type="button" value="Hacking" className="Info-FAQ-Select FAQ-Hacking-B FAQ-Unselected-B"/>
-                    <input type="button" value="Logistics" className="Info-FAQ-Select FAQ-Logistics-B FAQ-Unselected-B"/>
-                    <div className="scheduleInnards">
-                        <hr className="schedHR"></hr>
-                        <SchedElm start="10:15 PM" end="10:45 PM" name="ArchHacks thing!" loc="WashU"/>
-                        <SchedElm start="10:15 PM" end="10:45 PM" name="CTF event" loc="The Great Bear's Den"/>
-                        <SchedElm start="10:15 PM" end="10:45 PM" name="leedle lee" loc="The Internet"/>
-                        <SchedElm start="10:15 PM" end="10:45 PM" name="Awards" loc="WashU"/>
+                <div className="scheduleAndPic">
+                    <div className="schedule">
+                        <h1 className="scheduleTitle">Schedule</h1>
+                        <input type="button" value="General" className="Info-FAQ-Select FAQ-General-B FAQ-Selected-B"/>
+                        <input type="button" value="Hacking" className="Info-FAQ-Select FAQ-Hacking-B FAQ-Unselected-B"/>
+                        <input type="button" value="Logistics" className="Info-FAQ-Select FAQ-Logistics-B FAQ-Unselected-B"/>
+                        <div className="scheduleInnards">
+                            <hr className="schedHR"></hr>
+                            <SchedElm start="10:15 PM" end="10:45 PM" name="ArchHacks thing!" loc="WashU"/>
+                            <SchedElm start="10:15 PM" end="10:45 PM" name="CTF event" loc="The Great Bear's Den"/>
+                            <SchedElm start="10:15 PM" end="10:45 PM" name="leedle lee" loc="The Internet"/>
+                            <SchedElm start="10:15 PM" end="10:45 PM" name="Awards" loc="WashU"/>
+                        </div>
                     </div>
+                    <img src="../wuhack.jpg" alt="previous archhacks pic" />
                 </div>
                 <div className="social_media">
                         <a href="https://github.com/archhacks"><i class="fab fa-github fa-2x"></i></a>
