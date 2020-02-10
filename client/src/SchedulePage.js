cfb195b25e6948da49e255c1fcd0875a0f3bede7
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2020-02-09 20:22:47 -0600

    f

--- client/src/SchedulePage.js
+++ client/src/SchedulePage.js
@@ -52,7 +52,7 @@ class SchedulePage extends React.Component {
                             <SchedElm start="10:15 PM" end="10:45 PM" name="Awards" loc="WashU"/>
                         </div>
                     </div>
-                    <img src="../wuhack.jpg" alt="previous archhacks pic" />
+                    <img className="scheduleImage" src="../wuhack.jpg" alt="previous archhacks pic" />
                 </div>
                 <div className="social_media">
                         <a href="https://github.com/archhacks"><i class="fab fa-github fa-2x"></i></a>
