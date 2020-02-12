1e100b316f879bd292ba2207e86f1acd975adc9d
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2020-02-11 21:11:00 -0600

    dont like !

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -27,10 +27,10 @@ class LandingPage extends React.Component {
                 <h1 className="Landing-Title" id="heading"><span id="heading-gray">ARCH</span><span id="heading-red">HACKS</span></h1>
                 <p className="landing-gray">Washington University in St. Louis</p>
                 <p className="landing-gray">HealthTech</p>
-                {/* <button id="landing-applyButton">Apply for Exec!</button> */}
+                {/* <button id="landing-applyButton">Apply for Exec</button> */}
 
                 <form action="https://apply.archhacks.io">
-                    <input type="submit" value="Apply for Exec!" na   e="Subscribe" className="join-button" id="mc-embedded-subscribe"/>
+                    <input type="submit" value="Apply for Exec" na   e="Subscribe" className="join-button" id="mc-embedded-subscribe"/>
                 </form>
                 <a className="news-a" href="http://eepurl.com/gPL1Fr"><p className="newsletter">Want to join the newsletter too?</p>
                 <p className="newsletter">Sign up here.</p></a>
