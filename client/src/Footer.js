4a50146313b049b366d8fa4aebeb732f7102df7a
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2020-02-28 12:32:25 -0600

    thats that i guess

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -9,10 +9,13 @@ class Footer extends React.Component {
     render(){
         return (
             <div className="social_media">
-                    <a href="https://github.com/archhacks"><i class="fab fa-github fa-2x"></i></a>
-                    <a href="https://www.facebook.com/archhacks/"><i class="fab fa-facebook fa-2x"></i></a>
-                    <a href="https://twitter.com/archhacks"><i class="fab fa-twitter fa-2x"></i></a>
-                    <a href="https://medium.com/@ArchHacks"><i class="fab fa-medium-m fa-2x"></i></a>
+                    <div className="social_media_icons">
+                        <a href="https://github.com/archhacks"><i class="fab fa-github fa-2x"></i></a>
+                        <a href="https://www.facebook.com/archhacks/"><i class="fab fa-facebook fa-2x"></i></a>
+                        <a href="https://twitter.com/archhacks"><i class="fab fa-twitter fa-2x"></i></a>
+                        <a href="https://medium.com/@ArchHacks"><i class="fab fa-medium-m fa-2x"></i></a>
+                    </div>
+                    <p>info@archhacks.io</p>
             </div>
         )
     }
