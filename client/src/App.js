6a711a0cd16223c64418b6f5ec5250876a90571b
jackhoran60 <42753581+jackhoran60@users.noreply.github.com>
2020-02-28 11:43:43 -0600

    update

--- client/src/App.js
+++ client/src/App.js
@@ -33,6 +33,7 @@ function App() {
           <LandingPage />
           <InfoPage />
           {/* <SchedulePage /> */}
+          <Footer />
       </div>
     </div>
   );
--- client/src/SchedulePage.js
+++ client/src/SchedulePage.js
@@ -57,12 +57,7 @@ class SchedulePage extends React.Component {
                     </div>
                     <img className="scheduleImage" height="572px" width="505px" src="../wuhack.jpg" alt="previous archhacks pic"/>
                 </div>
-                <div className="social_media">
-                        <a href="https://github.com/archhacks"><i class="fab fa-github fa-2x"></i></a>
-                        <a href="https://www.facebook.com/archhacks/"><i class="fab fa-facebook fa-2x"></i></a>
-                        <a href="https://twitter.com/archhacks"><i class="fab fa-twitter fa-2x"></i></a>
-                        <a href="https://medium.com/@ArchHacks"><i class="fab fa-medium-m fa-2x"></i></a>
-                </div>
+                
             </div>
         )
     }
